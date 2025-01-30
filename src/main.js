e430c3909d0ea784c9b2a7c03e21a45ce6bb7803
Grace <gracesilvaduarte@gmail.com>
2025-01-29 19:26:15 -0500

    se cambian los nombres y colores de los temas

--- src/main.js
+++ src/main.js
@@ -9,28 +9,31 @@ const seccionContenido = document.getElementById("contentSection");
 /* botón de estado */
 const estado = document.getElementById("stateIcon");
 //let color = "#ff4000";
+/* botón para contraerMenu de color */
 const botonColor = document.getElementById("modeButton");
 
-function cambiar() {
+/* contraer la barra de navegación y expandir el contenido principal. rota el ícono del botón 180 grados */
+function contraerMenu() {
   botonMenu.classList.toggle("rotated");
   barraDeNavegacion.classList.toggle("hidden");
   seccionContenido.classList.toggle("expanded");
   //estado.style.backgroundColor=color;
 }
-function cambiarEstado() {
+// camnia el color del ícono de estado
+function cambiarEstado () {
   estado.classList.toggle("offline");
 }
 
-botonMenu.addEventListener("click", cambiar);
+botonMenu.addEventListener("click", contraerMenu);
 estado.addEventListener("click", cambiarEstado);
 
-
 botonColor.addEventListener("click", ()=>{
+  /* ícono dentro del botón, cambia de sol a luna */
   let toggleColorIcon = document.getElementById("toggleColorIcon");
 
-  toggleColorIcon.classList.toggle("fa-moon");
+  toggleColorIcon.classList.toggle("fa-moon");// clase propia de font awesome
   toggleColorIcon.classList.toggle("fa-sun");
 
-  /* cambio de todos los colores */
   document.body.classList.toggle("lightMode");
+
 });
